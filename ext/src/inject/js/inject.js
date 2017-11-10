f02626964b47f045d41468629c2c0c02bbb08e50
Andrei34 <andrei@planet34.org>
2017-11-10 03:32:21 +0100

    Rewrote folder detection system, refactored some code, added coments

--- ext/src/inject/js/inject.js
+++ ext/src/inject/js/inject.js
@@ -1,40 +1,68 @@
 // Folder coordinates
-const source = 'PRJ'
+const source = 'prj'
 const destination = '#box1'
-const parseUrl = /(http:|https:)\/\/(www\.)?(\w|\d)+.+?\//
+const parseUrl = /(http:|https:\/\/)((www\.)?(\w|\d)+.+?\/)/
+const parseReverseDomain = /\/(\w|\d)+.(\w|\d)+/
+
 const imageFiles = [ 'favicon.ico' ]
 
 
+// Draw bookmarks from folder to node #id
 function processData (folder, node) {
-  chrome.bookmarks.search(folder, function (res) {
-    if (res.length === 1) {
-      chrome.bookmarks.getChildren(res[0].id, function(res) {
-        if (res.length === 0) {
-          toggleSection('no-data')
-        } else {
-          flattenBookmarks(res, node)
-          toggleSection('content')
-        }
-      })
-    } else if (res.length > 1 ) {
-      toggleSection('duplicates')
-    } else {
-      toggleSection('no-data')
+    chrome.bookmarks.search(folder, function (res) {
+      if (res.length === 0) {
+        toggleSection('no-data')
+      }
+      else if (res.length >= 1) {
+        findFolder(res, renderBlock)
+      }
+    })
+}
+
+// Checks that there's only one folder with source name
+// If true returns else displays duplicates
+function findFolder (arr, callback) {
+  let results = []
+
+  for (elem of arr) {
+    if (elem.title.toLowerCase() === source.toLowerCase()) {
+      results.push(elem)
     }
+  }
+
+  if (results.length === 0) {
+    toggleSection('no-data')
+  }
+  else if (results.length === 1) {
+    chrome.bookmarks.getChildren(results[0].id, function (res) {
+      if (res.length === 0) {
+        toggleSection('no-bookmarks')
+      }
+      else {
+        // All good, draw the block
+        callback(results[0].id)
+      }
+    })
+  }
+  else if (results.length > 1){
+    toggleSection('duplicates')
+  }
+}
+
+// Draw a set of bookmarks from a folder
+function renderBlock (id) {
+  chrome.bookmarks.getChildren(id, function (res) {
+    makeCards(res, destination)
+    toggleSection('content')
   })
 }
 
-function drawCard(data, node) {
-  let domain = data.url.match(parseUrl)
-  $(node).append('<a href="'+ data.url + '" class="card" id="' + data.id + '"></a>')
-  $('#' + data.id).append('<img src="' + domain[0]+'favicon.ico">')
-}
-
-function flattenBookmarks (arr,node) {
+// Flatten array, draw elements
+function makeCards (arr, node) {
   for (elem of arr) {
     if (!elem.url) {
       chrome.bookmarks.getChildren(elem.id, function (res) {
-        flattenBookmarks(res, node)
+        makeCards(res, node)
       })
     } else {
       drawCard(elem, node)
@@ -42,8 +70,29 @@ function flattenBookmarks (arr,node) {
   }
 }
 
+// Draw an element
+function drawCard(data, node) {
+  let url = data.url.match(parseUrl)
+  let domain = getDomain(url[0])
+  let origin = url[1] + domain
+  
+  $(node).hide().append('<a href="'+ url[0] + '" class="card" id="' + data.id + '"></a>')
+
+
+  $('#' + data.id).append('<img src="' + origin +'favicon.ico">')
+
+  // $('#' + data.id).append('<img src="' + domain[0]+'favicon.ico">')
+}
+
 function toggleSection (id) {
   $('#' + id).toggleClass('hidden')
+}
+
+function getDomain (s) {
+    let rev = s.split("").reverse().join("")
+    let match = rev.match(parseReverseDomain)
+    let domain = match[0].split("").reverse().join("")
+    return domain
 }
 
 
