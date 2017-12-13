879f65377c481a5010e105c72f5a18e5afdb99e7
Andrei34 <andrei@planet34.org>
2017-12-13 23:17:08 +0100

    Removed obsolete code

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="list">
       <div class="list-header">
-        <p>{{folderName}}</p>
+        <p>{{this.listData.name}}</p>
       </div>
       <ul>
-        <li v-for="link in links">
+        <li v-for="link in listData.links">
           <a :href="link.url"
              :title="link.url">
             <img :src="'chrome://favicon/' + link.url">
@@ -16,20 +16,10 @@
 </template>
 
 <script>
-import { UiSelect } from 'keen-ui'
 
 export default {
-  created: function () {
-    let listName = Object.keys(this.listData)[0]
-    this.links = this.listData[listName]
-  },
-  components: {
-  },
-
   data () {
     return {
-      folderName: Object.keys(this.listData)[0],
-      links : []
     }
   },
 
