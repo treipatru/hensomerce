640a5d46e0ddd28fb4745618a5e0ac19c7e9a396
Andrei34 <andrei@planet34.org>
2017-12-29 18:13:56 +0100

    Indenting

--- src/plugins/packeryPlugin.js
+++ src/plugins/packeryPlugin.js
@@ -11,70 +11,59 @@ const CHANGE = 'itemChange'
 const REMOVE = 'itemRemoved'
 
 const packeryEvents = new Vue({})
-const packeryPlugin = () => {}
+const packeryPlugin = () => {
+}
 
 export default packeryPlugin
 
-packeryPlugin.install = function (Vue, options)
-{
-    Vue.directive('packery', {
-        bind (el, binding)
-        {
-            el.packery = new Packery(el, binding.value)
+packeryPlugin.install = function (Vue, options) {
+  Vue.directive('packery', {
+    bind(el, binding) {
+      el.packery = new Packery(el, binding.value)
 
-            const packeryDraw = node =>
-            {
-                if (!el.packery || !el.isSameNode(node))
-                {
-                    return
-                }
-                Vue.nextTick(() =>
-                {
-                    el.packery.reloadItems()
-                    el.packery.layout()
-                })
-            }
-
-            packeryEvents.$on(ADD, node =>
-            {
-                // Add to packery
-                let parent = node.parentNode
-                packeryDraw(parent)
-
-                // Register drag event
-                // let draggie = new Draggabilly( node )
-                // el.packery.bindDraggabillyEvents( draggie )
-            })
-
-            packeryEvents.$on(CHANGE, node =>
-            {
-                packeryDraw(node)
-            })
-
-            packeryEvents.$on(REMOVE, node =>
-            {
-                packeryDraw(node)
-            })
-        },
-        unbind (el)
-        {
-            el.packery.destroy()
-            el.packery = null
+      const packeryDraw = node => {
+        if (!el.packery || !el.isSameNode(node)) {
+          return
         }
-    })
+        Vue.nextTick(() => {
+          el.packery.reloadItems()
+          el.packery.layout()
+        })
+      }
 
-    Vue.directive('packeryItem', {
-        inserted (el)
-        {
-            packeryEvents.$emit(ADD, el)
-        },
-        componentUpdated (el)
-        {
-            packeryEvents.$emit(CHANGE, el.packeryNode)
-        },
-        unbind (el, binding, vnode)
-        {
-            packeryEvents.$emit(REMOVE, el.packeryNode)
-        }
-    })
+      packeryEvents.$on(ADD, node => {
+        // Add to packery
+        let parent = node.parentNode
+        packeryDraw(parent)
+
+        // Register drag event
+        // let draggie = new Draggabilly( node )
+        // el.packery.bindDraggabillyEvents( draggie )
+      })
+
+      packeryEvents.$on(CHANGE, node => {
+        packeryDraw(node)
+      })
+
+      packeryEvents.$on(REMOVE, node => {
+        packeryDraw(node)
+      })
+    },
+    unbind(el) {
+      el.packery.destroy()
+      el.packery = null
+    }
+  })
+
+  Vue.directive('packeryItem', {
+    inserted(el) {
+      packeryEvents.$emit(ADD, el)
+    },
+    componentUpdated(el) {
+      packeryEvents.$emit(CHANGE, el.packeryNode)
+    },
+    unbind(el, binding, vnode) {
+      packeryEvents.$emit(REMOVE, el.packeryNode)
+    }
+  })
 }
