5d77219224bdfff4944d7df41d051517af21b2dd
Andrei34 <andrei@planet34.org>
2018-01-03 18:56:03 +0100

    Removed menu transitions, code formatting

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -4,35 +4,40 @@
       <div class="col reset-space">
         <div class="menu-wrapper">
 
-          <menu-button v-on:windowToggle="windowToggle">
-          </menu-button>
+          <menu-button v-on:windowToggle="windowToggle"></menu-button>
 
-          <transition name="fade">
-            <div class="menu-actions"
-                  v-if="windowOpen">
-                  <a href="#"
-                     v-bind:class="['menu-item', viewActive === 'add' ? 'active' : '']"
-                     ref="add"
-                     v-on:click.prevent="emitButtonClick('add')">
-                    Add
-                    <ui-ripple-ink trigger="add"></ui-ripple-ink>
-                  </a>
-                  <a href="#"
-                     v-bind:class="['menu-item', viewActive === 'manage' ? 'active' : '']"
-                     ref="manage"
-                     v-on:click.prevent="emitButtonClick('manage')">
-                     Manage
-                     <ui-ripple-ink trigger="manage"></ui-ripple-ink>
-                  </a>
-                  <a href="#"
-                     ref="settings"
-                     v-bind:class="['menu-item', viewActive === 'settings' ? 'active' : '']"
-                     v-on:click.prevent="emitButtonClick('settings')">
-                     Settings
-                     <ui-ripple-ink trigger="settings"></ui-ripple-ink>
-                  </a>
+            <div
+              class="menu-actions"
+              v-if="windowOpen"
+            >
+              <a
+                href="#"
+                ref="add"
+                v-bind:class="['menu-item', viewActive === 'add' ? 'active' : '']"
+                v-on:click.prevent="emitButtonClick('add')"
+              >
+                Add
+                <ui-ripple-ink trigger="add"></ui-ripple-ink>
+              </a>
+              <a
+                href="#"
+                ref="manage"
+                v-bind:class="['menu-item', viewActive === 'manage' ? 'active' : '']"
+                v-on:click.prevent="emitButtonClick('manage')"
+              >
+                Manage
+                <ui-ripple-ink trigger="manage"></ui-ripple-ink>
+              </a>
+              <a
+                href="#"
+                ref="settings"
+                v-bind:class="['menu-item', viewActive === 'settings' ? 'active' : '']"
+                v-on:click.prevent="emitButtonClick('settings')"
+              >
+                Settings
+                <ui-ripple-ink trigger="settings"></ui-ripple-ink>
+              </a>
             </div>
-          </transition>
         </div>
       </div>
     </div>
