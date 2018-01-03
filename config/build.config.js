feb3c93d8ba9fdc0b4c47b8afb76cad020bbc565
Andrei34 <andrei@planet34.org>
2018-01-04 00:29:34 +0100

    Updated build

--- config/build.config.js
+++ config/build.config.js
@@ -1,9 +1,15 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseConfig = require('./base.config.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(baseConfig, {
   plugins: [
-    new UglifyJSPlugin()
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
   ]
 });
